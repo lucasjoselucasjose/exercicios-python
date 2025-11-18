bdbb250dc53cb56198d0a34f9831b099b8886c13
Raiane Cardoso <166438302+raianecardoso@users.noreply.github.com>
2025-11-18 14:50:25 +0000

    Corrige função test_type e melhora texto de retorno de test_options_resposta.

--- exercicios/01_essencial/002_divisao_03_e_05/test_002.py
+++ exercicios/01_essencial/002_divisao_03_e_05/test_002.py
@@ -6,14 +6,14 @@ def test_not_none():
     assert resposta(10) is not None, "Esperado valor diferente de 'None'"
 
 def test_type():
-    assert type(resposta(10)) == int, "Esperado um inteiro"
+    assert type(resposta(13)) == int, "Esperado um inteiro"
     assert type(resposta(15)) == str, "Esperado uma string"
 
 def test_parameters():
     assert len(inspect.getfullargspec(resposta).args) == 1, "Assinatura da função deverá receber um parâmetro"
 
 def test_options_resposta():
-    assert resposta(3) == 'Fizz' , f"Esperado Fizz"
-    assert resposta(5) == 'Buzz' , f"Esperado Buzz"
-    assert resposta(15) == 'FizzBuzz' , f"Esperado FizzBuzz"
+    assert resposta(3) == 'Fizz' , f"Esperado valor 'Fizz'"
+    assert resposta(5) == 'Buzz' , f"Esperado valor 'Buzz'"
+    assert resposta(15) == 'FizzBuzz' , f"Esperado valor 'FizzBuzz'"
     assert resposta(2) == 2, f" Esperando 2"
