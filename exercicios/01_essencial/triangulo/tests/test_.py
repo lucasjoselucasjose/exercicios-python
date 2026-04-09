9ed8d42082536a4ed68091da5bf573e9ca88e99c
raianecardoso <raianecardoso.rede@gmail.com>
2026-04-09 12:24:27 -0300

    Melhora testes do exercício triangulo.

--- exercicios/01_essencial/triangulo/tests/test_.py
+++ exercicios/01_essencial/triangulo/tests/test_.py
@@ -1,21 +1,22 @@
-from metro_milimetro.main import resposta
+from triangulo.main import resposta
 import inspect
 import pytest
 
 
 def test_not_none():
-    assert resposta(10) is not None, "Esperado valor diferente de 'None'"
+    assert resposta(10, 10, 10) is not None, "Esperado valor diferente de 'None'"
 
 
 def test_type():
-    assert type(resposta(13)) == int or type(resposta(3.2)) == float, "Esperado um inteiro ou float"
+    assert type(resposta(10, 10, 10)) == str, "Esperado uma string"
 
 
 def test_parameters():
-    assert len(inspect.getfullargspec(resposta).args) == 1, "Assinatura da função deverá receber um parâmetro"
+    assert len(inspect.getfullargspec(resposta).args) == 3, "Assinatura da função deverá receber três parâmetros"
 
 
 def test_options_resposta():
-    assert resposta(10) == 10000, f"Esperado valor 10000"
-    assert resposta(1.2) == 1200, f"Esperado valor 1200"
-    assert resposta(0.93) == 930, f"Esperado valor 930"
+    assert resposta(1, 2, 3) == "não forma triângulo", f"Esperado valor 'não forma triângulo'"
+    assert resposta(3, 4, 5) == "escaleno", f"Esperado valor 'escaleno'"
+    assert resposta(3, 3, 3) == "equilátero", f"Esperado valor 'equilátero'"
+    assert resposta(2, 2, 3) == "isósceles", f"Esperando valor 'isósceles'"
